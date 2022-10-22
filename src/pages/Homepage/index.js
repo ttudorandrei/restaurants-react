4ada4be37568ad31d3c1654c2884746aeec208f7
Tudor Tocan <tudor.tocan@gmail.com>
2022-10-22 10:17:34 +0100

    add home page, cleaned index.html

---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,25 @@
+const Homepage = () => {
+  return (
+    <div>
+      <div className="d-flex flex-column justify-content-center align-items-center vw-100">
+        <h1>Hello world</h1>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+          perspiciatis, alias dolores omnis quisquam eos ad laborum aut. Aliquam
+          iusto deserunt, mollitia, ad consequuntur, ab earum repudiandae
+          suscipit quo voluptatibus enim nisi magni aperiam. Iure architecto ea
+          unde voluptatibus cum pariatur obcaecati saepe id sit, assumenda
+          quidem maxime facilis magnam? Voluptatibus delectus ratione, tempora
+          ipsum cupiditate excepturi, perferendis quis incidunt saepe ullam illo
+          numquam aut consequuntur, molestiae amet accusamus itaque. Obcaecati,
+          temporibus? Et natus quo dolor, odio pariatur omnis assumenda incidunt
+          perspiciatis eaque unde reiciendis delectus ut adipisci veniam aut
+          accusamus expedita sit nulla ipsum nisi dignissimos? Ab, delectus
+          consectetur!
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
